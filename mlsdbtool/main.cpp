ed0d8ff9470a1926f77a7fe33d3e11edcffd20b8
Nokius <jupaja@gmx.net>
2016-10-25 14:36:10 +0200

    [mlsdbtool] add Bounding Box for Norway

--- mlsdbtool/main.cpp
+++ mlsdbtool/main.cpp
@@ -349,6 +349,15 @@ QMap<QString, BoundingBox> countryBoundingBoxes()
     netherlands_bb.upperRight.lon = 7.092;
     retn.insert(QLatin1String("Netherlands"), netherlands_bb);
 
+    BoundingBox norway_bb;
+    norway_bb.latShift = 0.0;
+    norway_bb.lonShift = 0.0;
+    norway_bb.lowerLeft.lat = 58.079;
+    norway_bb.lowerLeft.lon = 4.992;
+    norway_bb.upperRight.lat = 80.657;
+    norway_bb.upperRight.lon = 31.293;
+    retn.insert(QLatin1String("Norway"), norway_bb);
+
     BoundingBox india_bb;
     india_bb.latShift = 0.0;
     india_bb.lonShift = 0.0;
